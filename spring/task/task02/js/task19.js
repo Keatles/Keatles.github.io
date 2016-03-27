3f54aab6b901691d49f79159a4f6fd4942eb27bc
Keatles <373301137@qq.com>
2016-03-27 23:00:20 +0800

    update

--- spring/task/task02/js/task19.js
+++ spring/task/task02/js/task19.js
@@ -13,24 +13,30 @@ function leftInput(){
     var num = document.getElementById("input");
     var show = document.getElementById("show");
     var first = show.firstChild;
-    if(num.value) {
+    if(num.value>=10 && num.value<=100) {
         var elem = document.createElement("div");
         var text = document.createTextNode(num.value);
         elem.setAttribute("class","block");
         elem.appendChild(text);
         show.insertBefore(elem,first);
+    }else{
+        alert("请输入10-100之间的数字");
     }
+    histogram()
 }
 function rightInput(){
     var num = document.getElementById("input");
     var show = document.getElementById("show");
-    if(num.value) {
+    if(num.value>=10 && num.value<=100) {
         var elem = document.createElement("div");
         var text = document.createTextNode(num.value);
         elem.setAttribute("class","block");
         elem.appendChild(text);
         show.appendChild(elem);
+    }else{
+        alert("请输入10-100之间的数字");
     }
+    histogram()
 }
 function leftOut() {
     var show = document.getElementById("show");
@@ -44,6 +50,28 @@ function rightOut(){
     alert(last);
     show.removeChild(show.lastElementChild);
 }
+function histogram(){
+    {
+        var display = document.getElementsByClassName("block");
+        for (var i = 0; i < display.length; i++) {
+            display[i].style.width = 20+'px';
+            display[i].style.height = (display[i].innerHTML)*5 + 'px';
+        }
+    }
+}
+function bubbleSort(){
+    var display = document.getElementsByClassName("block");
+    for(var i=0;i<display.length;i++){
+        for(var j=0;j<display.length-1;j++){
+            if(parseInt(display[j].innerHTML)>parseInt(display[j+1].innerHTML)){
+                var text = display[j+1].innerHTML;
+                display[j+1].innerHTML = display[j].innerHTML;
+                display[j].innerHTML = text;
+                histogram()
+            }
+        }
+    }
+}
 
 /**
  * 绑定按钮
@@ -53,7 +81,10 @@ var lib = document.getElementById("input-left");
 var rib = document.getElementById("input-right");
 var lob = document.getElementById("out-left");
 var rob = document.getElementById("out-right");
+var bubble = document.getElementById("bubble");
 addEvent(lib,"click",leftInput);
 addEvent(rib,"click",rightInput);
 addEvent(lob,"click",leftOut);
 addEvent(rob,"click",rightOut)
+addEvent(bubble,"click",bubbleSort)
+window.onload = histogram();
