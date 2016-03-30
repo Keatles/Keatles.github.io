c4c4609aeb9078167b8118e1e37dbe734361fb39
Keatles <373301137@qq.com>
2016-03-30 14:31:53 +0800

    任务20

--- spring/task/task02/js/task20.js
+++ spring/task/task02/js/task20.js
@@ -10,26 +10,34 @@ function addEvent(element,action,func){
 
 //
 function leftInput(){
-    var num = document.getElementById("input");
+    var text = document.getElementById("input");
     var show = document.getElementById("show");
-    var first = show.firstChild;
-    if(num.value) {
-        var elem = document.createElement("div");
-        var text = document.createTextNode(num.value);
-        elem.setAttribute("class","block");
-        elem.appendChild(text);
-        show.insertBefore(elem,first);
+    var reCat = new RegExp("[^0-9A-Za-z\u4e00-\u9fa5]");
+    var t = text.value.split(reCat);
+    for(var i=0;i< t.length;i++) {
+        var first = show.firstChild;
+        if (t[i]) {
+            var elem = document.createElement("div");
+            var word = document.createTextNode(t[i]);
+            elem.setAttribute("class", "block");
+            elem.appendChild(word);
+            show.insertBefore(elem, first);
+        }
     }
 }
 function rightInput(){
-    var num = document.getElementById("input");
+    var text = document.getElementById("input");
     var show = document.getElementById("show");
-    if(num.value) {
-        var elem = document.createElement("div");
-        var text = document.createTextNode(num.value);
-        elem.setAttribute("class","block");
-        elem.appendChild(text);
-        show.appendChild(elem);
+    var reCat = new RegExp("[^0-9A-Za-z\u4e00-\u9fa5]");
+    var t = text.value.split(reCat);
+    for(var i=0;i< t.length;i++) {
+        if (t[i]) {
+            var elem = document.createElement("div");
+            var word = document.createTextNode(t[i]);
+            elem.setAttribute("class", "block");
+            elem.appendChild(word);
+            show.appendChild(elem);
+        }
     }
 }
 function leftOut() {
@@ -44,6 +52,25 @@ function rightOut(){
     alert(last);
     show.removeChild(show.lastElementChild);
 }
+function search(){
+    initBackgroundColor()
+    var show = document.getElementById("show");
+    var content = document.getElementById("search");
+    var reg = new RegExp(content.value);
+   for(var i=0;i<show.children.length;i++){
+    if(show.children[i].textContent.match(reg)){
+        show.children[i].style.background = "deepskyblue";
+    }else{
+        show.children[i].style.backgroundColor = "#cc8091"
+    }
+   }
+}
+function initBackgroundColor(){
+    var show = document.getElementById("show");
+    for(var i=0;i<show.children.length;i++){
+        show.children[i].style.backgroundColor = "#cc8091"
+    }
+}
 
 /**
  * 绑定按钮
@@ -53,7 +80,9 @@ var lib = document.getElementById("input-left");
 var rib = document.getElementById("input-right");
 var lob = document.getElementById("out-left");
 var rob = document.getElementById("out-right");
+var sea = document.getElementById("search-btn");
 addEvent(lib,"click",leftInput);
 addEvent(rib,"click",rightInput);
 addEvent(lob,"click",leftOut);
-addEvent(rob,"click",rightOut)
+addEvent(rob,"click",rightOut);
+addEvent(sea,"click",search);
